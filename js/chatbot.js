6ad402e3d18f4f274e6d04a85bcc856c824e3f24
“Aneesh <aneeshpanda.ap@gmail.com>
2021-08-06 11:16:59 +0530

    add: input box and menu for chatbot

--- js/chatbot.js
+++ js/chatbot.js
@@ -14,3 +14,33 @@ document.querySelector(".chatbot-nav-cross").addEventListener("click", () => {
         document.querySelector(".chatbot-wrap").setAttribute("style", "display: none;")
     }
 })
+document.querySelector(".chatbot-footer-up").addEventListener("click", () => {
+    if (document.querySelector("#chatbot-body").className == "chatbot-body-full") {
+        document.querySelector(".chatbot-footer-up").setAttribute("style", "transform: rotate(180deg);")
+        document.querySelector("#chatbot-body").className = "chatbot-body-small"
+        document.querySelector(".chatbot-menu").setAttribute("style", "display: block;")
+    }
+    else if (document.querySelector("#chatbot-body").className == "chatbot-body-small") {
+        document.querySelector(".chatbot-footer-up").setAttribute("style", "transform: rotate(0deg);")
+        document.querySelector("#chatbot-body").className = "chatbot-body-full"
+        document.querySelector(".chatbot-menu").setAttribute("style", "display: none;")
+    }
+})
+
+document.querySelector(".chatbot-send").addEventListener("click", () => {
+    console.log("Test")
+    document.querySelector("#chatbot-body").innerHTML += "<div class='text-right'><div class='chatbot-chat chatbot-chat-user'>" + document.querySelector("#chatbot-input-text").value + "</div></div>"
+    document.querySelector("#chatbot-input-text").value = ""
+    document.querySelector("#chatbot-body").scrollTo(0, (document.querySelector("#chatbot-body").scrollHeight))
+})
+
+function send(event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        if (document.querySelector("#chatbot-input-text").value != "") {
+            document.querySelector("#chatbot-body").innerHTML += "<div class='text-right'><div class='chatbot-chat chatbot-chat-user'>" + document.querySelector("#chatbot-input-text").value + "</div></div>"
+            document.querySelector("#chatbot-body").scrollTo(0, (document.querySelector("#chatbot-body").scrollHeight))
+            document.querySelector("#chatbot-input-text").value = "";
+        }
+    }
+}
