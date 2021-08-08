b568a9f76e679327361d5ee0181da19cd1f7acb6
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-08-08 22:25:56 +0530

    add: input box and menu for chatbot (#10)
    * add: input box and menu for chatbot
    
    * fix: top positioning of chatbot navbar
    
    * fix: chatbot navbar positioning
    
    * add: onclick focus, / to focus and esc to escape

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,6 +1,7 @@
 document.querySelector(".chatbot-button").addEventListener("click", () => {
     if (document.querySelector(".chatbot-wrap").getAttribute("style") == "display: none;") {
         document.querySelector(".chatbot-wrap").setAttribute("style", "display: block;")
+        document.querySelector("textarea").focus();
     }
     else if (document.querySelector(".chatbot-wrap").getAttribute("style") == "display: block;") {
         document.querySelector(".chatbot-wrap").setAttribute("style", "display: none;")
@@ -12,5 +13,53 @@ document.querySelector(".chatbot-nav-cross").addEventListener("click", () => {
     }
     else if (document.querySelector(".chatbot-wrap").getAttribute("style") == "display: block;") {
         document.querySelector(".chatbot-wrap").setAttribute("style", "display: none;")
+    }
+})
+document.querySelector(".chatbot-footer-menu-section").addEventListener("click", () => {
+    if (document.querySelector("#chatbot-body").className == "chatbot-body-full") {
+        document.querySelector(".chatbot-footer-up").setAttribute("style", "transform: rotate(180deg);")
+        document.querySelector("#chatbot-body").className = "chatbot-body-small"
+        document.querySelector(".chatbot-menu").setAttribute("style", "display: block;")
+    }
+    else if (document.querySelector("#chatbot-body").className == "chatbot-body-small") {
+        document.querySelector(".chatbot-footer-up").setAttribute("style", "transform: rotate(0deg);")
+        document.querySelector("#chatbot-body").className = "chatbot-body-full"
+        document.querySelector(".chatbot-menu").setAttribute("style", "display: none;")
+    }
+})
+
+document.querySelector(".chatbot-send").addEventListener("click", () => {
+    if (document.querySelector("#chatbot-input-text").value != "") {
+        document.querySelector("#chatbot-body").innerHTML += "<div class='text-right'><div class='chatbot-chat chatbot-chat-user'>" + document.querySelector("#chatbot-input-text").value + "</div></div>"
+        document.querySelector("#chatbot-input-text").value = ""
+        document.querySelector("#chatbot-body").scrollTo(0, (document.querySelector("#chatbot-body").scrollHeight))
+    }
+})
+
+function send(event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        if (document.querySelector("#chatbot-input-text").value != "") {
+            document.querySelector("#chatbot-body").innerHTML += "<div class='text-right'><div class='chatbot-chat chatbot-chat-user'>" + document.querySelector("#chatbot-input-text").value + "</div></div>"
+            document.querySelector("#chatbot-body").scrollTo(0, (document.querySelector("#chatbot-body").scrollHeight))
+            document.querySelector("#chatbot-input-text").value = "";
+        }
+    }
+}
+
+document.addEventListener("keydown", (event) => {
+    if(event.key == '/'){
+
+        if(document.activeElement.tagName != "TEXTAREA")
+        {
+            event.preventDefault();
+        }
+        if (document.querySelector(".chatbot-wrap").getAttribute("style") == "display: block;") {
+            document.querySelector("textarea").focus();
+        }
+    }
+
+    else if(event.key == "Escape"){
+        document.querySelector("textarea").blur();
     }
 })
