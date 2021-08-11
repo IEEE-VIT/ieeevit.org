b3e8d1541b0be75ba5105ce219a8f1f8b031cbb8
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-08-11 22:56:15 +0530

    feat: rebased integration with backend (#13)
    * feat: integration with backend
    
    * fix: scroll behaviour

--- js/main.js
+++ js/main.js
@@ -283,39 +283,4 @@ jQuery(document).ready(function($) {
 	};
 	siteSticky();
 
-	// navigation
-  var OnePageNavigation = function() {
-    var navToggler = $('.site-menu-toggle');
-   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top - 0
-      }, 1000, 'easeInOutCirc', function(){
-        window.location.hash = hash;
-      });
-
-    });
-  };
-  OnePageNavigation();
-
-  var siteScroll = function() {
-
-  	$(window).scroll(function() {
-
-  		var st = $(this).scrollTop();
-
-  		if (st > 100) {
-  			$('.js-sticky-header').addClass('shrink');
-  		} else {
-  			$('.js-sticky-header').removeClass('shrink');
-  		}
-
-  	}) 
-
-  };
-  siteScroll();
-
 });
