bd041ac0c70ed7d85a61bc3dc01adb0ee9c62ebd
Harsh Gupta <68965388+guptaharsh13@users.noreply.github.com>
2022-05-10 14:22:33 +0530

    feat: dark and light mode (#10)
    * feat: dark and light mode
    
    * fix: color in dark mode

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-window.onload = () => {
-  console.log("IEEE-VIT");
+const enableDarkMode = () => {
+  document.body.classList.add("dark");
+  localStorage.setItem("theme", "dark");
 };
+
+const enableLightMode = () => {
+  document.body.classList.remove("dark");
+  localStorage.setItem("theme", "light");
+};
+
+const changeTheme = () => {
+  let theme = localStorage.getItem("theme");
+  if (!theme) {
+    theme = "dark"; // because theme would be dark by default
+  }
+  if (theme === "dark") {
+    enableLightMode();
+  } else if (theme === "light") {
+    enableDarkMode();
+  } else {
+    console.error(
+      "Something went wrong - invalid value of theme in local storage"
+    );
+  }
+};
+
+document.getElementById("change-theme").addEventListener("click", () => {
+  changeTheme();
+});
