918541edd30e78e225aa3800026ded54f34ff7cc
Harsh Gupta <68965388+guptaharsh13@users.noreply.github.com>
2022-05-11 12:58:09 +0530

    fix: dark theme on load (#13)
    * fix: dark theme on load
    
    * detect system theme

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const enableDarkMode = () => {
-  document.body.classList.add("dark");
-  localStorage.setItem("theme", "dark");
-};
-
-const enableLightMode = () => {
-  document.body.classList.remove("dark");
-  localStorage.setItem("theme", "light");
-};
-
-const changeTheme = () => {
-  let theme = localStorage.getItem("theme");
-  if (!theme) {
-    theme = "dark"; // because theme would be dark by default
+const changeTheme = (theme) => {
+  if (!(theme === "dark" || theme === "light")) {
+    const dark_theme = window.matchMedia("(prefers-color-scheme: dark)");
+    theme = dark_theme.matches ? "dark" : "light";
   }
-  if (theme === "dark") {
-    enableLightMode();
-  } else if (theme === "light") {
-    enableDarkMode();
-  } else {
-    console.error(
-      "Something went wrong - invalid value of theme in local storage"
-    );
-  }
+  localStorage.setItem("theme", theme);
+  theme === "dark"
+    ? document.body.classList.add(theme)
+    : document.body.classList.remove("dark");
 };
 
 document.getElementById("change-theme").addEventListener("click", () => {
-  changeTheme();
+  let theme = localStorage.getItem("theme");
+  theme = theme === "dark" ? "light" : "dark";
+  changeTheme(theme);
 });
+
+window.onload = () => {
+  let theme = localStorage.getItem("theme");
+  changeTheme(theme);
+};
