1724e3212956bcb4a64f3dfcd676d2178a6d6980
Harsh Gupta <hg242322@gmail.com>
2022-08-10 22:07:58 +0530

    feat: header

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const changeTheme = (theme) => {
   theme === "dark"
     ? document.body.classList.add(theme)
     : document.body.classList.remove("dark");
+  changeImages(theme);
 };
 
 document.getElementById("change-theme").addEventListener("click", () => {
@@ -18,6 +19,15 @@ document.getElementById("change-theme").addEventListener("click", () => {
 const removeLoader = () => {
   document.getElementById("loader").style.display = "none";
   document.getElementById("main-content").style.display = "block";
+};
+
+const changeImages = (theme) => {
+  const shapes = document.querySelectorAll(".shape, .icon");
+  const to_replace = theme == "dark" ? "light" : "dark";
+
+  Array.from(shapes).forEach((shape) => {
+    shape.src = shape.src.replace(to_replace, theme);
+  });
 };
 
 window.onload = () => {
