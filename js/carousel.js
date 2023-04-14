bdfb85fdf6e3364d8693981c97e2c4b151517184
Shubhbansal9 <shubhbansal44@gmail.com>
2023-04-14 19:20:34 +0530

    feat: infinite scroll in carousels, fix: board positions corrected

--- js/carousel.js
+++ js/carousel.js
@@ -68,49 +68,134 @@ const previous = document.getElementsByClassName("previous");
 Array.from(previous).forEach((previous_button) => {
   previous_button.addEventListener("click", (event) => {
     const container = event.target.nextElementSibling;
-    container.scrollLeft -= container.children[0].offsetWidth;
+    const containerWidth = container.offsetWidth;
+    const currentScroll = container.scrollLeft;
+    const maxScroll = container.scrollWidth - containerWidth;
+    const previousScroll = currentScroll - containerWidth;
+
+    if (previousScroll < 0) {
+      container.scrollTo({ left: maxScroll, behavior: "smooth" });
+    } else {
+      container.scrollTo({ left: previousScroll, behavior: "smooth" });
+    }
   });
 });
 
 Array.from(document.querySelectorAll("#blogs, #podcast")).forEach((section) => {
   const container = section.querySelector(".carousel .container");
-  container.addEventListener("scroll", () => {
-    const previous_button = container.previousElementSibling;
+  const slides = container.querySelectorAll(".slide");
+  let currentIndex = 0;
+
+  Array.from(slides).forEach((slide, index) => {
+    if (slide.classList.contains("current")) {
+      currentIndex = index;
+    }
+  });
+
+  const previous_button = container.previousElementSibling;
+  const next_button = container.nextElementSibling;
+
+  function handleButtons() {
     if (screen.width <= 655) {
       return;
     }
-    if (container.scrollLeft == 0) {
+
+    if (currentIndex === 0) {
       previous_button.style.opacity = 0;
       previous_button.style.cursor = "default";
+    } else {
+      previous_button.style.opacity = 1;
+      previous_button.style.cursor = "pointer";
+    }
+
+    if (currentIndex === slides.length - 1) {
+      next_button.style.opacity = 0;
+      next_button.style.cursor = "default";
+    } else {
+      next_button.style.opacity = 1;
+      next_button.style.cursor = "pointer";
+    }
+  }
+
+  handleButtons();
+
+  container.addEventListener("scroll", () => {
+    Array.from(slides).forEach((slide, index) => {
+      if (slide.getBoundingClientRect().left >= 0 && slide.getBoundingClientRect().right <= window.innerWidth) {
+        currentIndex = index;
+      }
+    });
+
+    handleButtons();
+  });
+
+  previous_button.addEventListener("click", () => {
+    if (currentIndex === 0) {
       return;
     }
-    previous_button.style.opacity = 1;
-    previous_button.style.cursor = "pointer";
+
+    slides[currentIndex].classList.remove("current");
+    slides[currentIndex - 1].classList.add("current");
+    currentIndex--;
+
+    container.scrollTo({
+      left: slides[currentIndex].offsetLeft,
+      behavior: "smooth",
+    });
+
+    handleButtons();
+  });
+
+  next_button.addEventListener("click", () => {
+    if (currentIndex === slides.length - 1) {
+      return;
+    }
+
+    slides[currentIndex].classList.remove("current");
+    slides[currentIndex + 1].classList.add("current");
+    currentIndex++;
+
+    container.scrollTo({
+      left: slides[currentIndex].offsetLeft,
+      behavior: "smooth",
+    });
+
+    handleButtons();
   });
 });
+
 
 const next = document.getElementsByClassName("next");
 
 Array.from(next).forEach((next_button) => {
   next_button.addEventListener("click", (event) => {
     const container = event.target.previousElementSibling;
-    container.scrollLeft += container.children[0].offsetWidth;
+    const containerWidth = container.offsetWidth;
+    const currentScroll = container.scrollLeft;
+    const maxScroll = container.scrollWidth; // - containerWidth
+    const nextScroll = currentScroll + containerWidth;
+
+    if (nextScroll >= maxScroll) {
+      container.scrollTo({ left: 0, behavior: "smooth" });
+    } else {
+      container.scrollTo({ left: nextScroll, behavior: "smooth" });
+    }
 
     const previous_button = event.target.parentElement.children[0];
     previous_button.style.opacity = 1;
   });
 });
 
-const autoScroll = () => {
-  Array.from(next).forEach((next_button) => {
-    next_button.click();
-  });
-};
+// const autoScroll = () => {
+//   Array.from(next).forEach((next_button) => {
+//     next_button.click();
+//   });
+// };
 
-const scroller = setInterval(() => {
-  autoScroll();
-}, 3525);
+// const scroller = setInterval(() => {
+//   autoScroll();
+// }, 5525);
 
-setTimeout(() => {
-  clearInterval(scroller);
-}, 15325);
+// setTimeout(() => {
+//   clearInterval(scroller);
+// }, 15325);
