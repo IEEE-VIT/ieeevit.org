f87bcdff7924bc1fc3e97ef93c72ddb6a09f253e
scar027 <shantanupai@hotmail.com>
2024-12-30 12:07:47 +0530

    fix: refactoring and support for 2024 alumni board
    Signed-off-by: scar027 <shantanupai@hotmail.com>

--- js/alumni.js
+++ js/alumni.js
@@ -19,11 +19,15 @@ function changeImage() {
   var textContainer = document.getElementById("text-container");
   var text = "Executive board 2021 - 22";
   if (selectedValue === "image-grid1") {
-    text = "Executive board 2020 - 21";
-  } else if (selectedValue === "image-grid2") {
     text = "Executive board 2019 - 20";
+  } else if (selectedValue === "image-grid2") {
+    text = "Executive board 2020 - 21";
   } else if (selectedValue === "image-grid3") {
     text = "Executive board 2021 - 22";
+  } else if (selectedValue === "image-grid4") {
+    text = "Executive board 2022 - 23";
+  } else if (selectedValue === "image-grid5") {
+    text = "Executive board 2023 - 24";
   }
   textContainer.textContent = text;
 }
